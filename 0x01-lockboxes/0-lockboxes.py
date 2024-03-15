b9770665a72b6854e6cc91359bc26a309fc4e7bb
Spesho-clei <jepkoechnancy56@gmail.com>
2024-03-15 08:38:17 +0000

    Lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -7,21 +7,18 @@ def canUnlockAll(boxes):
     if not boxes or not boxes[0]:
         return False
 
-    n1 = len(boxes)
-    visited = [False] * n1
-    visited[0] = True
-    queue = [0]
+    n = len(boxes)
+    keys = set(boxes[0])
+    opened = {0}
 
+    # Perform BFS starting from the first box
+    queue = [0]
     while queue:
         current_box = queue.pop(0)
-        print("Visiting box:", current_box)
-
         for key in boxes[current_box]:
-            if 0 <= key < n1 and not visited[key]:
-                print("Found key to box:", key)
-                visited[key] = True
+            if key not in opened:
+                keys |= set(boxes[key])
+                opened.add(key)
                 queue.append(key)
-            else:
-                print("Skipping key to box:", key)
 
-    return all(visited)
+    return len(opened) == n
